4c3002747f4d1fe72c42440cc6320953fb22f412
rayleigh420 <nhatduy0409@gmail.com>
2023-01-19 20:54:35 +0700

    Test react router dom v6

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -1,9 +1,28 @@
 import { useState } from 'react'
+import { Routes, Route, Link } from 'react-router-dom'
 
 function App() {
     return (
-        <div className="App">
-        </div>
+        <>
+            <div className="App">
+                <nav>
+                    <ul>
+                        <li>
+                            <Link to='/home'>Home</Link>
+                        </li>
+                        <li>
+                            <Link to='/contact'>Contact</Link>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+            <Routes>
+                <Route path='/home' />
+                <Route path='/contact' />
+            </Routes>
+        </>
+
+        // <h2>HEllo</h2>
     )
 }
 
