0ebf6ec80ab13ff49f75fee7a35c85d9d4f7675d
rayleigh420 <nhatduy0409@gmail.com>
2023-02-02 20:22:18 +0700

    Fix something about cors and creadentical

--- BackEnd/src/index.js
+++ BackEnd/src/index.js
@@ -3,44 +3,48 @@ import cors from "cors";
 import cookieParser from "cookie-parser";
 import mongoose from "mongoose";
 import initRoutes from "./routes/routes.js";
+import credentials from './middleware/credenticals'
+import corsOptions from './config/corsOptions'
 
 require("dotenv").config();
 
 const app = express();
 const port = process.env.PORT || 8080;
 
-app.use(function (req, res, next) {
-  // Website you wish to allow to connect
-  res.setHeader(
-    "Access-Control-Allow-Origin",
-    "http://localhost:5173"
-  );
+app.use(credentials);
 
-  // Request methods you wish to allow
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
-  );
+// app.use(function (req, res, next) {
+//   // Website you wish to allow to connect
+//   res.setHeader(
+//     "Access-Control-Allow-Origin",
+//     "http://localhost:5173"
+//   );
 
-  // Request headers you wish to allow
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "X-Requested-With,content-type"
-  );
+//   // Request methods you wish to allow
+//   res.setHeader(
+//     "Access-Control-Allow-Methods",
+//     "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+//   );
 
-  // Set to true if you need the website to include cookies in the requests sent
-  // to the API (e.g. in case you use sessions)
-  res.setHeader("Access-Control-Allow-Credentials", true);
+//   // Request headers you wish to allow
+//   res.setHeader(
+//     "Access-Control-Allow-Headers",
+//     "X-Requested-With,content-type"
+//   );
 
-  // Pass to next layer of middleware
-  next();
-});
+//   // Set to true if you need the website to include cookies in the requests sent
+//   // to the API (e.g. in case you use sessions)
+//   res.setHeader("Access-Control-Allow-Credentials", true);
+
+//   // Pass to next layer of middleware
+//   next();
+// });
 
 mongoose.connect(process.env.MONGOOSE_URL, () => {
   console.log("Connect Mongoose Databse success!");
 });
 
-// app.use(cors());
+app.use(cors(corsOptions));
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
