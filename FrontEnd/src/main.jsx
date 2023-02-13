0d4176707f6716b6f8040ad9ca70f68a04378b7d
rayleigh420 <nhatduy0409@gmail.com>
2023-02-13 09:55:16 +0700

    Disable react-dev-tools

--- FrontEnd/src/main.jsx
+++ FrontEnd/src/main.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from "react-router-dom"
+import { disableReactDevTools } from '@fvilers/disable-react-devtools';
 import App from './App'
 import './main.css';
 import 'antd/dist/reset.css';
 import { AuthProvider } from './context/AuthProvider';
+
+// if (process.env.NODE_ENV === 'production') {
+// }
+disableReactDevTools();
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
